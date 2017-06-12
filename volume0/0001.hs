502553d49c1acf093ae65efbbce79e886e2512c5
shitaro <apprentice.physicist.shitaro@gmail.com>
2017-06-12 15:03:56 +0900

    Accepted List of Top 3 Hills

---
+++ volume0/0001.hs
@@ -0,0 +1,14 @@
+-- List of Top 3 Hills
+import Control.Monad  -- replicateM
+import Data.Ord       -- sortBy (comparing Down)
+import Data.List      -- intercalate
+
+main =
+  replicateM 10 getLine >>= putStrLn . intercalate "\n" . map show . solve
+
+solve :: [String] -> [Int]
+solve = take 3 . sortBy (comparing Down) . map read
+
+-- import Control.Monad
+-- replicateM :: Monad m => Int -> m a -> m [a]
+-- perform the action n times, gathering the results
