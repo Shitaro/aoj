8a2aa49c0367bbadb078bebbe0b37cfabcf0f9dc
shitaro <apprentice.physicist.shitaro@gmail.com>
2017-06-12 15:58:16 +0900

    With unlines

--- volume0/0001.hs
+++ volume0/0001.hs
@@ -4,7 +4,7 @@ import Data.Ord       -- sortBy (comparing Down)
 import Data.List      -- intercalate
 
 main =
-  replicateM 10 getLine >>= putStrLn . intercalate "\n" . map show . solve
+  replicateM 10 getLine >>= putStr . unlines . map show . solve
 
 solve :: [String] -> [Int]
 solve = take 3 . sortBy (comparing Down) . map read
