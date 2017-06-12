777eb8721e5ddc9b2e88f708baa1f2148774564d
shitaro <apprentice.physicist.shitaro@gmail.com>
2017-06-12 12:51:21 +0900

    With unwords

--- itp1/1c.hs
+++ itp1/1c.hs
@@ -3,4 +3,4 @@ import Control.Applicative
 
 main = do
   [a,b] <- (map read . words) <$> getLine :: IO [Int]
-  putStrLn $ show (a*b) ++ " " ++ show (2*(a+b))
+  putStrLn . unwords $ map show [a*b, 2*(a+b)]
